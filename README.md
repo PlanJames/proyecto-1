e1f2d194f320597c5844f2a46d6137be91ac8af0
Florencia <mfromero05@gmail.com>
2024-09-09 09:38:16 -0300

    Primero commit de usuarios para pushear

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 proyecto-1
+
+Archivos de texto plano en CSV.
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,14 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        // Mensaje para indicar que el programa está en espera
+        System.out.println("Esperando a crearlo juntos");
     }
 }
+
