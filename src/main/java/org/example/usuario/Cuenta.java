ff9fdc6f5a3031344055b94143ebfa572db022d1
Florencia <mfromero05@gmail.com>
2024-09-20 18:25:14 -0300

    Cambios clase transaccion

--- src/main/java/org/example/usuario/Cuenta.java
+++ src/main/java/org/example/usuario/Cuenta.java
@@ -75,12 +75,16 @@ public class Cuenta {
 
     // Restar saldo en la cuenta
     public void restarSaldo(double cantidad) {
-        if (cantidad <= 0) {
-            throw new IllegalArgumentException("La cantidad a restar debe ser mayor que cero.");
+        try {
+            if (cantidad <= 0) {
+                throw new IllegalArgumentException("La cantidad a restar debe ser mayor que cero.");
+            }
+            if (saldo < cantidad) {
+                throw new IllegalArgumentException("Fondos insuficientes en la cuenta.");
+            }
+            saldo -= cantidad;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error al restar saldo: " + e.getMessage());
         }
-        if (saldo < cantidad) {
-            throw new IllegalArgumentException("Fondos insuficientes en la cuenta.");
-        }
-        saldo -= cantidad;
     }
 }
